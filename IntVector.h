4d6ec24153e7536ac90ebc0ad78e403fff0941d7
DimaVor1 <91839889+DimaVor1@users.noreply.github.com>
2021-10-23 17:43:02 +0300

    Update IntVector.h

--- IntVector.h
+++ IntVector.h
@@ -7,6 +7,46 @@
 
 namespace P3_1 {
 
+	//////////////////////////////////////////////////////////////////////////////
+	//				class and methods			    //			
+	//////////////////////////////////////////////////////////////////////////////
+
+	class IntVector{
+	private: 
+		static const int SZ = 10;							// maximum vector size
+		int cSZ;									// current size			
+		int ar[SZ];									// array of vector elements
+	public:
+		IntVector(): cSZ(0) {};								// default constructor
+		IntVector(int el);								// 1 element constructor 
+		IntVector(int size, const int *ar);						// elements massive constructor
+		
+		std::ostream& print(std::ostream&, int a, int l) const;				// output to stream and selection part of a vector with given length
+		friend std::ostream& operator <<(std::ostream&, const IntVector&);		// << overload	
+						
+		void sum(const IntVector &);							// sum vectors (v0+=v1)
+		IntVector& operator +=(const IntVector&);					// += overload
+
+		void addEl(int el);								// including a new element in a vector
+		void sort(int first, int last);							// quicksort vector
+		int maxNum() const;								// finding maximum element
+
+		//////////////////////////////////////////////////////////////////////////
+		//	auxiliary methods and overloads at the request of the teacher	//			
+		//////////////////////////////////////////////////////////////////////////
+
+		IntVector operator -();								// - overload
+		friend IntVector operator+(const IntVector& iv1, const IntVector& iv2);		// + overload
+		int& operator[] (const int index);						// [] overload				
+		IntVector& operator++();							// pre++ overload
+		IntVector operator++(int);							// post++ overload
+
+		int getArEl(int i) { return ar[i]; }						// getters
+		int getCSz() { return cSZ; }							// getters
+		~IntVector() = default;								// destructor
+	};
+
+
 	template <class T>
 	int getNum(T& a) {
 		std::cin >> a;
@@ -19,26 +59,5 @@ namespace P3_1 {
 		}
 		return 1;
 	}
-
-	class IntVector{
-	private: 
-		static const int SZ = 10;
-		int cSZ;
-		int ar[SZ];
-	public:
-		IntVector(): cSZ(0) {};
-		IntVector(int el);
-		IntVector(int size, int *ar);
-
-		std::ostream& print(std::ostream&, int a, int l) const;
-
-		int getArEl(int i) { return ar[i]; }
-		int getCSz() { return cSZ; }
-
-		void sum(IntVector);
-		void addEl(int el);
-		void sort(int first, int last);
-		int maxNum() const;
-	};
 }
 #endif
